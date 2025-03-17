a5977d54b251767d391ca6afe0d4aaecf116c7eb
chapeullah <chapeullah@gmail.com>
2025-03-17 07:43:09 +0300

    Update files

--- HTTPServer.java
+++ HTTPServer.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.Socket;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.net.ServerSocket;
@@ -15,14 +17,14 @@ public class HTTPServer {
     public static void main(String[] args) {
         try {
             serverSocket = new ServerSocket(PORT);
-            System.out.println("Server started on " + IP + ":" + PORT);
+            System.out.println(timeNow() + " Server started on " + IP + ":" + PORT);
 
             new Thread(() -> {
                 try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
                     String command = reader.readLine();
-                    if ("shutdown".equalsIgnoreCase(command)) {
-                        stopServer();
-                    }
+                    if ("help".equalsIgnoreCase(command)) {
+                        System.out.println(timeNow() + " Commands: \n- shutdown");
+                    } else if ("shutdown".equalsIgnoreCase(command)) stopServer();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -32,33 +34,26 @@ public class HTTPServer {
                 Socket socket = null;
                 try {
                     socket = serverSocket.accept();
-                    System.out.println("Client connected: " + socket.getInetAddress());
                     
                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
                     String requestLine;
-                    System.out.println("=== RECEIVED REQUEST ===");
-                    while ((requestLine = reader.readLine()) != null && !requestLine.isEmpty()) {
-                        System.out.println(requestLine);
-                    }
-                    System.out.println("=== END OF REQUEST ===");
+                    System.out.println(timeNow() + " === RECEIVED REQUEST FROM " + socket.getInetAddress() + " ===");
+                    while ((requestLine = reader.readLine()) != null && !requestLine.isEmpty()) System.out.println(timeNow() + " " + requestLine);
+                    System.out.println(timeNow() + " === END OF REQUEST " + socket.getInetAddress() + " ===");
 
-                    writer.write("HTTP/1.1 200 OK\r\n");
-                    writer.write("Content-Type: text/plain\r\n");
-                    writer.write("Content-Length: 13\r\n");
+                    String responseBody = "<html><body><h1>Server's response</h1></body></html>";
+                    writer.write("HTTP/1.1 200 OKr\n");
+                    writer.write("Content-Type: text/html; charset=UTF-8\r\n");
+                    writer.write("Content-Length: " + responseBody.length() + "\r\n");
+                    writer.write("Connection: close\r\n");
                     writer.write("\r\n");
-                    writer.write("Hello, world!");
-                    writer.flush();
-
-                    InputStream inputStream = socket.getInputStream();
-                    while (true) {
-                        int data = inputStream.read(); 
-                        if (data == -1) {
-                            System.out.println("Client disconnected: " + socket.getInetAddress());
-                            socket.close();
-                            break;
-                        }
+                    writer.write(responseBody);
+                    try {
+                        writer.flush();
+                    } catch (IOException e) {
+                        System.out.println(timeNow() + "Client closed connection before receiving response.");
                     }
                 } catch (IOException e) {
                     if (!running) break;
@@ -91,5 +86,9 @@ public class HTTPServer {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static String timeNow () {
+        return "[" + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "]";
     }
 }
